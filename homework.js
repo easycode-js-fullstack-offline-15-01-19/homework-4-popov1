efedbae3402c423b5990848f6a34a954f972ee38
popov2evgeniy2 <popov2evgeniy2@gmail.com>
2019-02-03 15:54:20 +0200

    Added task 5

--- homework.js
+++ homework.js
@@ -1,6 +1,7 @@
 // -------------------------- Home work - 4--------------------------
 // -------------------------- Попов Евгений --------------------------
 
+// Task 4
 /**
  * @task Create a multiply function that will take any number
  *  of numbers and return their product:
@@ -181,4 +182,222 @@ function getUsersByParameters (usersArr, parameter, value) {
     }
 
     return result;
+}
+
+
+// Task 5
+/**
+ * @description function getNewString() - remove spaces in string and 
+ * start each word from uppercase letter
+ * @param {Array} inputArr 
+ * @returns {String} return new string
+ */
+function getNewString(inputArr) {
+    let resArr = [];
+    for (let i = 0, max = inputArr.length; i < max; i++) {
+        resArr.push(inputArr[i][0].toUpperCase() + inputArr[i].substr(1));
+    }
+    return resArr.join('');
+}
+
+/**
+ * @description function getNewStringFromArray() - create new string from array values, each value * 10
+ * @param {Array} inputArr 
+ * @returns {String} return new string
+ */
+function getNewStringFromArray(inputArr) {
+    let result = [];
+    for (let i = 0, max = inputArr.length; i < max; i++) {
+        result.push(inputArr[i] * 10);
+    }
+    return result.join(', ');
+}
+
+/**
+ * @description function getNewStringFromArray() - create new string from array of objects,
+ * where each object have structure {name: nameValue, age: ageValue}
+ * @param {Array} inputArr 
+ * @returns {String} return new string
+ */
+function getNewStringFromObject(inputObj) {
+    let result = [];
+    for (let o of inputObj) {
+        result.push(`${o.name} is ${o.age}`);
+    }
+    return result.join(', ');
+}
+
+/**
+ * @description function getReverseArray() - create new string from array,
+ * convert each value to its reverse value.
+ * To process data this function uses reverseString() function
+ * @param {Array} inputArr 
+ * @returns {String} return new string
+ */
+function getReverseArray(inputArray) {
+    let result = [];
+    for (let i = 0, max = inputArray.length; i < max; i++) {
+        result.push(reverseString(inputArray[i]));
+    }
+    return result.join(', ');
+}
+
+/**
+ * @description function processData() - process data from the input array using special functions,
+ * @param {Array} inputArr - input array. Can use next models:
+ *  - [{string1}, {string2}, {string3}, {string4}, ...]
+ *  - [{number1}, {number2}, {number3}, ...]
+ *  - [{age: ageValue1, name: namevalue1}, {age: ageValue2, name: namevalue2}, ...]
+ * @param {Function} func - function which proccess input data
+ * @returns {String} return new string
+ */
+function processData (inputArr, func = funcName) {
+    return 'New value: ' + func(inputArr);
+}
+
+let stringArray = ['my', 'name', 'is', 'Trinity'];
+let numberArray = [10, 20, 30];
+let objectArray = [{age: 45, name: 'Jhon'}, {age: 20, name: 'Aaron'}];
+let arrayToReverse = ['abc', '123'];
+
+console.log(processData(stringArray, getNewString));
+console.log(processData(numberArray, getNewStringFromArray));
+console.log(processData(objectArray, getNewStringFromObject));
+console.log(processData(arrayToReverse, getReverseArray));
+
+
+
+/**
+ * @task Write an analogue of the every method.
+ * Create the function every, it must take an array of numbers as the first argument
+ * (be sure to check that the array is passed) with the second argument callback
+ * the function must return true or false depending on the result of the callback
+ * call (check a number greater than 5).
+ *  A callback must accept one element of the array, its index in the array, and the entire array.
+ * @param {Array} arr  - input array
+ * @param {Function} callback  - function to process data from array
+ */
+function myEvery (arr, callback) {
+    if (!Array.isArray(arr)) {
+        return 'Error! First argument not array.';
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (!callback(arr[i], i, arr)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+let arrNumbers = [10, 12, 13, 4, 9, 56, 0, 77];
+myEvery(arrNumbers, function (number, index, arr2) {
+    return number > 5;
+});
+
+//-------------
+let checkArrNumbers = [1,2,3,5,8,9,10];
+let newEveryResult = [];
+checkArrNumbers.forEach (function (number, index, array) {
+    let res = {};
+    res.digit = number;
+    if (!(number % 2)) {
+        res.odd = true;
+    } else {
+        res.odd = false;
+    }
+    newEveryResult.push(res);
+});
+
+//-------------
+
+let zeroArray = [12, 4, 50, 1, 0, 18, 40];
+let r2 = zeroArray.every(function (number, index, array) {
+    return number !== 0;
+});
+
+
+let wordsArray = ['yes', 'hello', 'no', 'easycode', 'what'];
+let r3 = wordsArray.some(function (value, index, array) {
+    return value.length > 3;
+});
+
+//-------------
+
+let wordsArray2 = [{char:"a",index:12}, {char:"w",index:8}, {char:"Y",index:10}, {char:"p",index:3}, {char:"p",index:2},
+{char:"N",index:6}, {char:" ",index:5}, {char:"y",index:4}, {char:"r",index:13}, {char:"H",index:0},
+{char:"e",index:11}, {char:"a",index:1}, {char:" ",index:9}, {char:"!",index:14}, {char:"e",index:7}];
+
+
+
+function compareNumeric(a, b) {
+    if (a.index > b.index) return 1;
+    if (a.index < b.index) return -1;
+}
+
+function getStringFromArray2 (inputArray) {
+    let sortArr = inputArray.sort(compareNumeric);
+    let newStr2 = '';
+    for (let i= 0, max = sortArr.length; i < max; i++) {
+        newStr2 += sortArr[i].char;
+    }
+    return newStr2;
+}
+
+//-------------
+
+let checkArr3 =  [[14, 45], [1], ['a', 'c', 'd']];
+function compareNumeric2(a, b) {
+    if (a.length > b.length) return 1;
+    if (a.length < b.length) return -1;
+}
+
+let sortCheckArr3 = checkArr3.sort(compareNumeric2);
+
+//-------------
+
+let checkArr4 = [
+    {cpu: 'intel', info: {cores:2, сache: 3}},
+    {cpu: 'intel', info: {cores:4, сache: 4}},
+    {cpu: 'amd', info: {cores:1, сache: 1}},
+    {cpu: 'intel', info: {cores:3, сache: 2}},
+    {cpu: 'amd', info: {cores:4, сache: 2}}
+];
+
+function compareNumeric3(a, b) {
+    if (a.info.cores > b.info.cores) return 1;
+    if (a.info.cores < b.info.cores) return -1;
+}
+
+let sortCheckArr4 = checkArr4.sort(compareNumeric3);
+
+//----------
+
+let products = [
+    {title: 'prod1', price: 5.2},
+    {title: 'prod2', price: 0.18},
+    {title: 'prod3', price: 15},
+    {title: 'prod4', price: 25},
+    {title: 'prod5', price: 18.9},
+    {title: 'prod6', price: 8},
+    {title: 'prod7', price: 19},
+    {title: 'prod8', price: 63}
+];
+
+function compareNumeric4(a, b) {
+    if (a['price'] > b['price']) return 1;
+    if (a['price'] < b['price']) return -1;
+}
+
+function filterCollection (productsArr, min, max) {
+    let resArr = [];
+    for (let i = 0, max2 = productsArr.length; i < max2; i++) {
+        console.log(productsArr[i]['price']);
+        if (productsArr[i]['price'] >= min && productsArr[i]['price'] <= max) {
+            resArr.push(productsArr[i]);
+        }
+    }
+
+    return resArr.sort(compareNumeric4);
 }
